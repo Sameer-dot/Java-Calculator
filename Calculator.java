cd94d04945001762ebcdd84e51e203161ddfe733
Sameer-dot <sameernaveed37@gmail.com>
2022-03-31 03:24:45 +0500

    Final Commit!

--- Calculator.java
+++ Calculator.java
@@ -11,7 +11,7 @@ class MyBtn extends JButton{
         this.setFocusable(false);
         this.setBackground(new Color(0xF1F1F1));
         this.setText(str);
-        Font  f2  = new Font(Font.SANS_SERIF,Font.PLAIN, 18);
+        Font  f2  = new Font(Font.SANS_SERIF,Font.PLAIN, 20);
         this.setFont(f2);
         this.setForeground(new Color(0x505050));
     }
@@ -25,6 +25,7 @@ public class Calculator extends JFrame implements ActionListener {
     String operator;
     Boolean flag = false;
     Boolean flamg = true;
+    Boolean flg = true;
 
 
     Calculator(){
@@ -35,18 +36,13 @@ public class Calculator extends JFrame implements ActionListener {
         this.setResizable(false);
         this.setBackground(new Color(0xE6E6E6));
 
-        //JPanel panel = new JPanel();
-        //panel.setBounds(0,0,390,160);
-        //panel.setBackground(Color.GRAY);
-        //this.add(panel);
-
         textField = new JTextField();
         textField.setBounds(0,0,385,40);
         textField.setBackground(new Color(0xE6E6E6));
         textField.setBorder(null);
         textField.setEditable(false);
         textField.setHorizontalAlignment(JTextField.RIGHT);
-        textField.setFont(new Font(Font.SANS_SERIF,Font.PLAIN,14));
+        textField.setFont(new Font(Font.SANS_SERIF,Font.PLAIN,18));
         this.add(textField);
 
         tf = new JTextField("0");
@@ -77,51 +73,51 @@ public class Calculator extends JFrame implements ActionListener {
 
         btn[8] = new MyBtn("7");
         btn[8].setBackground(Color.WHITE);
-        btn[8].setFont(new Font(Font.SANS_SERIF, Font.BOLD, 15));
+        btn[8].setFont(new Font(Font.SANS_SERIF, Font.BOLD, 20));
         btn[8].setForeground(new Color(0x0f0f0f));
 
         btn[9] = new MyBtn("8");
         btn[9].setBackground(Color.WHITE);
-        btn[9].setFont(new Font(Font.SANS_SERIF, Font.BOLD, 15));
+        btn[9].setFont(new Font(Font.SANS_SERIF, Font.BOLD, 20));
         btn[9].setForeground(new Color(0x0f0f0f));
 
         btn[10] = new MyBtn("9");
-        btn[10].setFont(new Font(Font.SANS_SERIF, Font.BOLD, 15));
+        btn[10].setFont(new Font(Font.SANS_SERIF, Font.BOLD, 20));
         btn[10].setBackground(Color.WHITE);
         btn[10].setForeground(new Color(0x0f0f0f));
 
         btn[11] = new MyBtn("x");
 
         btn[12] = new MyBtn("4");
-        btn[12].setFont(new Font(Font.SANS_SERIF, Font.BOLD, 15));
+        btn[12].setFont(new Font(Font.SANS_SERIF, Font.BOLD, 20));
         btn[12].setBackground(Color.WHITE);
         btn[12].setForeground(new Color(0x0f0f0f));
 
         btn[13] = new MyBtn("5");
         btn[13].setBackground(Color.WHITE);
-        btn[13].setFont(new Font(Font.SANS_SERIF, Font.BOLD, 15));
+        btn[13].setFont(new Font(Font.SANS_SERIF, Font.BOLD, 20));
         btn[13].setForeground(new Color(0x0f0f0f));
 
         btn[14] = new MyBtn("6");
         btn[14].setBackground(Color.WHITE);
-        btn[14].setFont(new Font(Font.SANS_SERIF, Font.BOLD, 15));
+        btn[14].setFont(new Font(Font.SANS_SERIF, Font.BOLD, 20));
         btn[14].setForeground(new Color(0x0f0f0f));
 
         btn[15] = new MyBtn("-");
 
         btn[16] = new MyBtn("1");
-        btn[16].setFont(new Font(Font.SANS_SERIF, Font.BOLD, 15));
+        btn[16].setFont(new Font(Font.SANS_SERIF, Font.BOLD, 20));
         btn[16].setBackground(Color.WHITE);
         btn[16].setForeground(new Color(0x0f0f0f));
 
         btn[17] = new MyBtn("2");
         btn[17].setBackground(Color.WHITE);
-        btn[17].setFont(new Font(Font.SANS_SERIF, Font.BOLD, 15));
+        btn[17].setFont(new Font(Font.SANS_SERIF, Font.BOLD, 20));
         btn[17].setForeground(new Color(0x0f0f0f));
 
         btn[18] = new MyBtn("3");
         btn[18].setBackground(Color.WHITE);
-        btn[18].setFont(new Font(Font.SANS_SERIF, Font.BOLD, 15));
+        btn[18].setFont(new Font(Font.SANS_SERIF, Font.BOLD, 20));
         btn[18].setForeground(new Color(0x0f0f0f));
 
         btn[19] = new MyBtn("+");
@@ -129,7 +125,7 @@ public class Calculator extends JFrame implements ActionListener {
 
         btn[21] = new MyBtn("0");
         btn[21].setBackground(Color.WHITE);
-        btn[21].setFont(new Font(Font.SANS_SERIF, Font.BOLD, 15));
+        btn[21].setFont(new Font(Font.SANS_SERIF, Font.BOLD, 20));
         btn[21].setForeground(new Color(0x0f0f0f));
 
         btn[22] = new MyBtn(".");
@@ -146,15 +142,17 @@ public class Calculator extends JFrame implements ActionListener {
         this.repaint();
     }
 
-
+    //Main Method
     public static void main(String[] args){
         Calculator cal = new Calculator();
     }
 
+    //Set History Text Field
     public void setHistoryField(String val){
         textField.setText(tf.getText().concat(val));
     }
 
+    //Set Text Field Function
     public void setTextField(String val){
         if(flamg){
             tf.setText("");
@@ -165,12 +163,14 @@ public class Calculator extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        //%
         if (e.getSource() == btn[0]){
             num1 = Double.parseDouble(tf.getText());
             result = num1/100;
             tf.setText(String.valueOf(result));
             textField.setText("");
         }
+        //sqrt
         else if (e.getSource() == btn[1]){
             num1 = Double.parseDouble(tf.getText());
             result = Math.sqrt(num1);
@@ -179,6 +179,7 @@ public class Calculator extends JFrame implements ActionListener {
             textField.setText(val);
             flag=true;
         }
+        //sqr
         else if (e.getSource() == btn[2]){
             num1 = Double.parseDouble(tf.getText());
             result = num1 * num1;
@@ -187,24 +188,32 @@ public class Calculator extends JFrame implements ActionListener {
             textField.setText(val);
             flag=true;
         }
+        //1/x
         else if (e.getSource() == btn[3]){
-            num1 = Double.parseDouble(tf.getText());
-            result = 1/num1;
-            tf.setText(String.valueOf(result));
-            String val = "1/("+String.valueOf(num1)+")";
-            textField.setText(val);
-            flag=true;
+            try {
+                num1 = Double.parseDouble(tf.getText());
+                result = 1 / num1;
+                tf.setText(String.valueOf(result));
+                String val = "1/(" + String.valueOf(num1) + ")";
+                textField.setText(val);
+                flag = true;
+            }
+            catch(ArithmeticException exc){
+                tf.setText("Cannot Divide by 0");
+            }
         }
         //CE
         else if (e.getSource() == btn[4]){
             tf.setText("0");
             flamg=true;
+            flg=true;
         }
         //C
         else if (e.getSource() == btn[5]){
             tf.setText("0");
             textField.setText("");
             flamg=true;
+            flg=true;
         }
         //esc
         else if (e.getSource() == btn[6]){
@@ -223,11 +232,14 @@ public class Calculator extends JFrame implements ActionListener {
         //Divide
         else if (e.getSource() == btn[7]){
             try {
-                num1 = Double.parseDouble(tf.getText());
-                operator = "÷";
-                String val = String.valueOf(btn[7].getText());
-                setHistoryField(val);
-                tf.setText("");
+                String v = textField.getText();
+                if(! v.contains("÷")) {
+                    num1 = Double.parseDouble(tf.getText());
+                    operator = "÷";
+                    String val = String.valueOf(btn[7].getText());
+                    setHistoryField(val);
+                    tf.setText("");
+                }
             }catch(ArithmeticException excp){
                 tf.setText("Cannot Divide by 0");
             }
@@ -267,11 +279,14 @@ public class Calculator extends JFrame implements ActionListener {
         }
         //multiply
         else if (e.getSource() == btn[11]){
-            num1 = Double.parseDouble(tf.getText());
-            operator = "x";
-            String val = String.valueOf(btn[11].getText());
-            setHistoryField(val);
-            tf.setText("");
+            String v = textField.getText();
+            if(! v.contains("x")) {
+                num1 = Double.parseDouble(tf.getText());
+                operator = "x";
+                String val = String.valueOf(btn[11].getText());
+                setHistoryField(val);
+                tf.setText("");
+            }
         }
         //4
         else if (e.getSource() == btn[12]){
@@ -308,11 +323,14 @@ public class Calculator extends JFrame implements ActionListener {
         }
         //subtract
         else if (e.getSource() == btn[15]){
-            num1 = Double.parseDouble(tf.getText());
-            operator = "-";
-            String val = String.valueOf(btn[15].getText());
-            setHistoryField(val);
-            tf.setText("");
+            String v = textField.getText();
+            if(! v.contains("-")) {
+                num1 = Double.parseDouble(tf.getText());
+                operator = "-";
+                String val = String.valueOf(btn[15].getText());
+                setHistoryField(val);
+                tf.setText("");
+            }
         }
         //1
         else if (e.getSource() == btn[16]){
@@ -349,11 +367,14 @@ public class Calculator extends JFrame implements ActionListener {
         }
         //plus
         else if (e.getSource() == btn[19]){
-            num1 = Double.parseDouble(tf.getText());
-            operator = "+";
-            String val = String.valueOf(btn[19].getText());
-            setHistoryField(val);
-            tf.setText("");
+            String v = textField.getText();
+            if(! v.contains("+")) {
+                num1 = Double.parseDouble(tf.getText());
+                operator = "+";
+                String val = String.valueOf(btn[19].getText());
+                setHistoryField(val);
+                tf.setText("");
+            }
         }
         //+/-
         else if (e.getSource() == btn[20]){
@@ -380,26 +401,36 @@ public class Calculator extends JFrame implements ActionListener {
             }
         }
         else if (e.getSource() == btn[23]){
-            num2 = Double.parseDouble(tf.getText());
-            String val = String.valueOf(num1) + " "+ String.valueOf(operator) + " " +String.valueOf(num2) + " =";
-            textField.setText(val);
+            if(flg) {
+                num2 = Double.parseDouble(tf.getText());
+                String val = String.valueOf(num1) + " " + String.valueOf(operator) + " " + String.valueOf(num2) + " =";
+                textField.setText(val);
+                flg = false;
+            }
+            else{
+                String val = String.valueOf(num1) + " " + String.valueOf(operator) + " " + String.valueOf(num2) + " =";
+                textField.setText(val);
+            }
             switch (operator){
                 case "+":
                     result = num1+num2;
+                    flamg=true;
                     break;
                 case "-":
                     result = num1-num2;
+                    flamg=true;
                     break;
                 case "x":
                     result = num1*num2;
+                    flamg=true;
                     break;
                 case "÷":
                     result = num1/num2;
+                    flamg=true;
                     break;
             }
             tf.setText(String.valueOf(result));
             num1=result;
-
         }
     }
 }
